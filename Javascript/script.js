603986b8c2abad325b90259452f56777156240f4
CodeWithShaman <shamanaliteevino@gmail.com>
2025-03-17 00:37:36 +0500

    Blub Assignment On OFF

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,28 +1,26 @@
 let imageBd = document.getElementById("img1");
 let imgDb = document.getElementById("img2");
-let btnClick = document.getElementById("btn2"); 
-let btnHide = document.getElementById("btn3");
-let container = document.getElementsByClassName("container");
-// container.style.display = 'none' 
+let btnClick = document.getElementById("btn2");
 
-function on() {
-  imageBd.setAttribute("src", "Images/w-blub.png");
-  imageBd.style.width = "300px";
-  imageBd.style.height = "320px";
-  imageBd.style.display = "block";
-  imgDb.style.display = "none";
-  btnClick.style.display = "none"
-  btnHide.style.display = "block"
+function toggleBulb() {
+    if (btnClick.innerText === "ON") {
+        btnClick.innerText = "OFF";
+        imageBd.setAttribute("src", "Images/w-blub.png");
+        imageBd.style.width = "300px";
+        imageBd.style.height = "320px";
+        imageBd.style.display = "block";
+        imgDb.style.display = "none";
+        btnClick.style.backgroundColor = 'red'
+        btnClick.style.color = 'white'
 
-
-}
-function off() {
-    imgDb.setAttribute(
-    "src", "Images/blub.png");
-  imgDb.style.width = "340px";
-  imgDb.style.height = "290px";
-  imageBd.style.display = "none";
-  imgDb.style.display = "block";
-  btnHide.style.display = "none"
-  btnClick.style.display = "block"
+    } else {
+        btnClick.innerText = "ON"; // Change button text
+        imgDb.setAttribute("src", "Images/blub.png");
+        imgDb.style.width = "340px";
+        imgDb.style.height = "290px";
+        imageBd.style.display = "none";
+        imgDb.style.display = "block";
+        btnClick.style.backgroundColor = 'green'
+        btnClick.style.color = 'white'
+    }
 }
